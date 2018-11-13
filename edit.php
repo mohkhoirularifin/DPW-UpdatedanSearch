cead8d73f2d67293748db8ee2c2715aea4935f7a
Moh. Khoirul Arifin <karifin998@gmail.com>
2018-11-13 09:18:53 +0700

    Menambahkan HTML pada edit.php

--- edit.php
+++ edit.php
@@ -25,3 +25,46 @@
         }
     }
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Update data</title>
+    </head>
+    <body>
+        <h1>Update Data Mahasiswa</h1>
+        <form action="" method="post">
+            <ul>
+                <li>
+                    <!-- for pada label terhubung dengan id jadi jika label nama diklik maka textfield nama akan aktif juga -->
+                    <label for="Nama">Nama:</label>
+                    <!-- untuk mengisian name besar kecilnya harus sesuai dengan fieldnya -->
+                    <input type="text" name="Nama" id="Nama" >
+                </li>
+                <li>
+                    <label for="Nim">NIM :</label>
+                    <input type="text" name="Nim" id="Nim" required>
+                </li>
+                <li>
+                    <label for="Email">Email</label>
+                    <input type="text" name="Email" id="Email" required>
+                </li>
+                <li>
+                    <label for="Jurusan">Jurusan</label>
+                    <input type="text" name="Jurusan" id="Jurusan" required>
+                </li>
+                <li>
+                    <label for="Gambar">Gambar</label>
+                    <input type="text" name="Gambar" id="Gambar" required>
+                </li>
+                <li>
+                    <button type="submit" name="submit"> Update </button>
+                </li>
+            </ul>
+        </form>
+    </body>
+</html>
